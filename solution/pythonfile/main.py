ab2d83a5ccd10c39388d40dbe93295148cb6c9b1
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-09-18 22:53:58 +0700

    Latest update 18/09

--- solution/pythonfile/main.py
+++ solution/pythonfile/main.py
@@ -18,58 +18,96 @@ t11 = 0.250
 t12 = 0.360
 t22 = 0.047
 
-a_lattice = 31.29
+a_lattice = 3.129
 hbar = constants.hbar
 B = 0
 e = 9.1e-31
 n = (e / (8 * hbar)) * B * a_lattice**2 * sqrt(3)
-N = 200
+N = 100
 L = 4 * pi * sqrt(3) / (3 * a_lattice)
 
 
 def eigenvalue():
     eigenvalues = []
+    H = np.zeros((3, 3), dtype=complex)
     Lamb1 = np.zeros((N, N))
     Lamb2 = np.zeros((N, N))
     Lamb3 = np.zeros((N, N))
-    kx = np.zeros((N, N))
-    ky = np.zeros((N, N))
-    dki = 4 * pi * sqrt(3) / (3 * (N - 1) * a_lattice)
-    dkj = 4 * pi * sqrt(3) / (3 * (N - 1) * a_lattice)
+
+    ak1 = np.zeros(N)
+    ak2 = np.zeros(N)
+    G = 4 * pi / (sqrt(3) * a_lattice)
+
+    k1min = -G / 2
+    k1max = G / 2
+    k2min = -G / 2
+    k2max = G / 2
+
+    dk1 = (k1max - k1min) / (N - 1)
+    dk2 = (k2max - k2min) / (N - 1)
+
+    for i in range(N):
+        ak1[i] = k1min + i * dk1
+
+    for j in range(N):
+        ak2[j] = k2min + j * dk2
+
+    akx = np.zeros((len(ak1), len(ak2)))
+    aky = np.zeros((len(ak1), len(ak2)))
+
     for i in range(N):
         for j in range(N):
-            kx[i][j] = cos(pi / 6) * (i * dki + j * dkj) - 2 * pi / (a_lattice)
-            # ky[i][j] = sin(pi / 6) * (-i * dki + j * dkj) + 0
-            ky[i][j] = 0
-            a = kx[i][j] * a_lattice / 2
-            b = sqrt(3) / 2 * ky[i][j] * a_lattice
+            akx[i][j] = sqrt(3) / 2 * (ak1[i] + ak2[j])
+            aky[i][j] = -1 / 2 * (ak1[i] - ak2[j])
 
-            h0 = 2 * t0 * (cos(2 * a) + 2 * cos(a) * cos(b)) + e1
-            h1 = 2j * t1 * (sin(2 * a) + sin(a) * cos(b)) - 2 * sqrt(3) * t2 * sin(a) * sin(b)
-            h2 = 2 * t2 * (cos(2 * a) - cos(a) * cos(b)) + 2j * sqrt(3) * t1 * cos(a) * sin(b)
-            h11 = (t11 + 3 * t22) * cos(a) * cos(b) + 2 * t11 * cos(2 * a) + e2
-            h22 = (3 * t11 + t22) * cos(a) * cos(b) + 2 * t22 * cos(2 * a) + e2
-            h12 = 4j * t12 * (sin(a) * cos(a) - sin(a) * cos(b)) + sqrt(3) * (t22 - t11) * sin(a) * sin(b)
-            h1dag = np.conjugate(h1)
-            h2dag = np.conjugate(h2)
-            h12dag = np.conjugate(h12)
+            a = akx[i][j] / 2 * a_lattice
+            b = sqrt(3) / 2 * aky[i][j] * a_lattice
 
-            H = np.array([[h0, h1, h2], [h1dag, h11, h12], [h2dag, h12dag, h22]])
-            fk = L**2 * sqrt(3) * dki * dkj * H / (8 * pi**2)
-            w = LA.eigvalsh(H)
+            H[0][0] = 2 * t0 * (cos(2 * a) + 2 * cos(n) * cos(a) * cos(b) + 2 * sin(n) * sin(a) * cos(b)) + e1
+            H[0][1] = 2j * t1 * (sin(2 * a) + cos(n) * sin(a) * cos(b) - sin(n) * cos(a) * cos(b)) - 2 * sqrt(3) * t2 * (
+                cos(n) * sin(a) * sin(b) - sin(n) * cos(a) * sin(b)
+            )
+            H[0][2] = 2 * t2 * (cos(2 * a) - cos(n) * cos(a) * cos(b) - sin(n) * sin(a) * cos(b)) + 2j * sqrt(3) * t1 * (
+                cos(n) * cos(a) * sin(b) + sin(n) * sin(a) * sin(b)
+            )
+            H[1][1] = (t11 + 3 * t22) * (cos(n) * cos(a) * cos(b) + sin(n) * sin(a) * cos(b)) + 2 * t11 * cos(2 * a) + e2
+            H[2][2] = (t22 + 3 * t11) * (cos(n) * cos(a) * cos(b) + sin(n) * sin(a) * cos(b)) + 2 * t22 * cos(2 * a) + e2
+            H[1][2] = 4j * t12 * (sin(a) * cos(a) - cos(n) * sin(a) * cos(b) + sin(n) * cos(a) * cos(b)) + sqrt(3) * (t22 - t11) * (
+                cos(n) * sin(a) * sin(b) - sin(n) * cos(a) * sin(b)
+            )
+            H[1][0] = np.conjugate(H[0][1])
+            H[2][0] = np.conjugate(H[0][2])
+            H[2][1] = np.conjugate(H[1][2])
+
+            # fk = L**2 * sqrt(3) * dki * dkj * H / (8 * pi**2)
+            w, _ = LA.eigh(H)
             eigenvalues.append(w)
             Lamb1[i, j] = float(w[0])
             Lamb2[i, j] = float(w[1])
             Lamb3[i, j] = float(w[2])
 
-    plt.figure(figsize=(16, 8))
+    print(akx[0][0])
+    print(akx[8][0])
+    print(aky[1][0])
+    print(aky[8][8])
+    if akx[0][0] == sqrt(3) / 2 * (k2min + k1min):
+        print(True)
+    # np.savetxt("e.txt", Lamb1, header="1")
+    fig = plt.figure(figsize=(16, 8))
+    plt.subplot(1, 3, 1)
+    ax = fig.add_subplot(121, projection="3d")
+    ax.plot_surface(aky, akx, Lamb1, cmap="viridis")
+    ax.plot_surface(aky, akx, Lamb2, cmap="plasma")
+    ax.plot_surface(aky, akx, Lamb3, cmap="inferno")
 
-    plt.plot(kx, Lamb1)
-    plt.plot(kx, Lamb2)
-    plt.plot(kx, Lamb3)
+    plt.subplot(1, 3, 3)
+    plt.plot(akx, Lamb1)
+    plt.plot(akx, Lamb2)
+    plt.plot(akx, Lamb3)
+
     plt.grid(True)
-    plt.xlabel("k-point")
-    plt.ylabel("Energy (eV)")
+    plt.xlabel("kx")
+    plt.ylabel("ky")
 
     plt.show()
 
@@ -79,4 +117,4 @@ if __name__ == "__main__":
     a = eigenvalue()
     end = time.time()
     timing = end - start
-    print(timing)
+    print(f"{timing}sscond")
