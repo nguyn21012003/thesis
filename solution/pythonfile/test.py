a1ae2516d8ce45b5f3710f93e89476d98e6b9394
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-09-18 22:53:30 +0700

    final test.py. Do not change

--- solution/pythonfile/test.py
+++ solution/pythonfile/test.py
@@ -28,74 +28,57 @@ N = 100
 
 def eigenvalue():
     eigenvalues = []
-    eigenvectors = []
-    kx = np.zeros(N)
-    ky = np.zeros(N)
-    for i in range(0, N):
-        dk = 4 * pi / (a_lattice * (N + 1))
-        kx[0] = -2 * pi / a_lattice
-        if i + 1 < N:
-            kx[i + 1] = kx[i] + dk
-        ky[i] = 0
+    L1 = np.zeros((N, N))
+    L2 = np.zeros((N, N))
+    L3 = np.zeros((N, N))
+    kx = np.zeros((N, N))
+    ky = np.zeros((N, N))
+    dki = 4 * pi * sqrt(3) / (3 * (N - 1) * a_lattice)
+    dkj = 4 * pi * sqrt(3) / (3 * (N - 1) * a_lattice)
+    for i in range(N):
+        for j in range(N):
+            kx[i][j] = cos(pi / 6) * (i * dki + j * dkj) - 2 * pi / (a_lattice)
+            ky[i][j] = sin(pi / 6) * (-i * dki + j * dkj) + 0
+            # ky[i][j] = 0
+            a = kx[i][j] * a_lattice / 2
+            b = sqrt(3) / 2 * ky[i][j] * a_lattice
 
-        a = 1 / 2 * kx[i] * a_lattice
-        b = sqrt(3) / 2 * ky[i] * a_lattice
+            h0 = 2 * t0 * (cos(2 * a) + 2 * cos(a) * cos(b)) + e1
+            h1 = 2j * t1 * (sin(2 * a) + sin(a) * cos(b)) - 2 * sqrt(3) * t2 * sin(a) * sin(b)
+            h2 = 2 * t2 * (cos(2 * a) - cos(a) * cos(b)) + 2j * sqrt(3) * t1 * cos(a) * sin(b)
+            h11 = (t11 + 3 * t22) * cos(a) * cos(b) + 2 * t11 * cos(2 * a) + e2
+            h22 = (3 * t11 + t22) * cos(a) * cos(b) + 2 * t22 * cos(2 * a) + e2
+            h12 = 4j * t12 * (sin(a) * cos(a) - sin(a) * cos(b)) + sqrt(3) * (t22 - t11) * sin(a) * sin(b)
+            h1dag = np.conjugate(h1)
+            h2dag = np.conjugate(h2)
+            h12dag = np.conjugate(h12)
 
-        h0 = 2 * t0 * (cos(2 * a) + 2 * cos(n) * cos(a) * cos(b) + 2 * sin(n) * sin(a) * cos(b)) + e1
-        h1 = 2j * t1 * (sin(2 * a) + cos(n) * sin(a) * cos(b) - sin(n) * cos(a) * cos(b)) - 2 * sqrt(3) * t2 * (
-            cos(n) * sin(a) * sin(b) - sin(n) * cos(a) * sin(b)
-        )
-        h2 = 2 * t2 * (cos(2 * a) - cos(n) * cos(a) * cos(b) - sin(n) * sin(a) * cos(b)) + 2j * sqrt(3) * t1 * (
-            cos(n) * cos(a) * sin(b) + sin(n) * sin(a) * sin(b)
-        )
-        h11 = (t11 + 3 * t22) * (cos(n) * cos(a) * cos(b) + sin(n) * sin(a) * cos(b)) + 2 * t11 * cos(2 * a) + e2
-        h22 = (t22 + 3 * t11) * (cos(n) * cos(a) * cos(b) + sin(n) * sin(a) * cos(b)) + 2 * t22 * cos(2 * a) + e2
-        h12 = 4j * t12 * (sin(a) * cos(a) - cos(n) * sin(a) * cos(b) + sin(n) * cos(a) * cos(b)) + sqrt(3) * (t22 - t11) * (
-            cos(n) * sin(a) * sin(b) - sin(n) * cos(a) * sin(b)
-        )
-        h1dag = np.conjugate(h1)
-        h2dag = np.conjugate(h2)
-        h12dag = np.conjugate(h12)
+            H = np.array([[h0, h1, h2], [h1dag, h11, h12], [h2dag, h12dag, h22]])
+            w, v = LA.eigh(H)
+            eigenvalues.append(w)
+            L1[i, j] = float(w[0])
+            L2[i, j] = float(w[1])
+            L3[i, j] = float(w[2])
 
-        H = np.array([[h0, h1, h2], [h1dag, h11, h12], [h2dag, h12dag, h22]])
-        w = LA.eigvalsh(H)
-        eigenvalues.append(w)
+            # print(L1[i][j], kx[i][j], ky[i][j], i, j, "\n")
 
-    with open("eigenvalue.txt", "w", newline="") as writefile:
-        header = ["lambda1", "lambda2", "lambda3"]
-        writer = csv.DictWriter(writefile, fieldnames=header)
-        writer.writeheader()
-        for i in range(len(eigenvalues)):
-            writer.writerow({"lambda1": eigenvalues[i][0], "lambda2": eigenvalues[i][1], "lambda3": eigenvalues[i][2]})
+    fig = plt.figure(figsize=(16, 8))
+    ax = fig.add_subplot(121, projection="3d")
+    ax.plot_surface(kx, ky, L1, cmap="viridis")
+    ax.plot_surface(kx, ky, L2, cmap="plasma")
+    ax.plot_surface(kx, ky, L3, cmap="inferno")
 
-    k_points = np.linspace(-2 * pi / a_lattice, 2 * pi / a_lattice, N)
-    lambda1 = []
-    lambda2 = []
-    lambda3 = []
-    for ev in eigenvalues:
-        lambda1.append([ev[0]])
-        lambda2.append([ev[1]])
-        lambda3.append([ev[2]])
+    plt.subplot(1, 3, 3)
+    plt.plot(kx, L1)
+    plt.plot(kx, L2)
+    plt.plot(kx, L3)
 
-    energies = np.array([lambda1, lambda2, lambda3])
-    plt.figure(figsize=(10, 6))
-    for i in range(len(energies)):
-        plt.plot(k_points, energies[i, :], label=f"Band {i + 1}")
+    plt.grid(True)
+    plt.xlabel("kx")
+    plt.ylabel("ky")
 
-    plt.xlabel("k-point")
-    plt.ylabel("Energy (eV)")
-
-    plt.legend()
     plt.show()
 
 
-def main():
-    start = time.time()
-    eigenvalue()
-    # plot("eigenvalue.txt")
-    end = time.time()
-    print(end - start, "time")
-
-
 if __name__ == "__main__":
-    main()
+    eigenvalue()
